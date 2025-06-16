f8973178ba62511abf73f08fbdf11ef7a11cf46d
maniahania <astrzelecka99@gmail.com>
2025-06-16 20:36:24 +0200

    copy website layout

--- angular/first-project/src/app/app.component.ts
+++ angular/first-project/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFilterComponent } from './product-filter/product-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductFilterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
