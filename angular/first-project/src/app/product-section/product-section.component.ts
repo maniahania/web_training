012101d26d05a8528b307cb382634f8e14e65452
maniahania <astrzelecka99@gmail.com>
2025-07-19 19:22:13 +0200

    add ng-content element

--- angular/first-project/src/app/product-section/product-section.component.ts
+++ angular/first-project/src/app/product-section/product-section.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductFilterComponent } from './product-filter/product-filter.component';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
   selector: 'app-product-section',
-  imports: [ProductListComponent, ProductFilterComponent],
+  imports: [ProductListComponent, ProductFilterComponent, CardComponent],
   templateUrl: './product-section.component.html',
   styleUrl: './product-section.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
