e410e7968d50f54500a9bac93c9ed71696192e60
maniahania <astrzelecka99@gmail.com>
2025-07-21 17:44:53 +0200

    update subscription in details

--- angular/first-project/src/app/product-section/product-details/product-details.component.ts
+++ angular/first-project/src/app/product-section/product-details/product-details.component.ts
@@ -1,13 +1,16 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   inject,
+  OnInit,
   signal,
 } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../product.model';
 import { CurrencyPipe } from '@angular/common';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -17,19 +20,27 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './product-details.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   private readonly productService = inject(ProductService);
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
 
   product = signal<Product | undefined>(undefined);
 
-  constructor() {
-    this.activatedRoute.paramMap.subscribe((paramMap) => {
-      const productId = paramMap.get('productId');
-      if (!productId) return;
-      this.productService.getProductById(productId).subscribe((product) => {
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.paramMap
+      .pipe(
+        switchMap((paramMap) => {
+          const productId = paramMap.get('productId') ?? '';
+          return this.productService.getProductById(productId);
+        })
+      )
+      .subscribe((product) => {
         this.product.set(product);
       });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
     });
   }
 }
