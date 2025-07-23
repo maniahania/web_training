cef133f47c25c08aa0195fa7e22af24314f316af
maniahania <astrzelecka99@gmail.com>
2025-07-23 20:57:49 +0200

    empty state

--- angular/first-project/src/app/header/header.component.ts
+++ angular/first-project/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { LanguageService } from '../shared/services/language.service';
+import { EmptyStateComponent } from '../shared/components/empty-state/empty-state.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [EmptyStateComponent],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
--- angular/first-project/src/app/nav-section/nav-section.component.ts
+++ angular/first-project/src/app/nav-section/nav-section.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NavDataService } from '../shared/services/nav-data.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { EmptyStateComponent } from '../shared/components/empty-state/empty-state.component';
 
 @Component({
   selector: 'app-nav-section',
-  imports: [],
+  imports: [EmptyStateComponent],
   standalone: true,
   templateUrl: './nav-section.component.html',
   styleUrl: './nav-section.component.css',
